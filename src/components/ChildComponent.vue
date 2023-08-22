34b02dbad07619fe8569619768a1d7c4cbcb48fe
mmrmilon14 <mmrmilon14@gmail.com>
2023-08-22 10:05:22 +0600

    File added

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+   const props  = defineProps(['message']);
+   const emit   = defineEmits(['update:modelValu']);
+
+   function messageEmitted (){
+     props.name = emit('update:message','Hi I\'m a Web Developer with Vue Learner!')
+   }
+
+</script>
+
+<template>
+    <div class="mt-10">
+        <h1 class="text-black-900 text-xl my-5">Chiled Component:</h1>
+        <h2 class="text-black text-xl my-5">Message : <span class="text-yellow-600 hover:text-yellow-900">{{ message }}</span></h2>
+
+        <button @click="messageEmitted ()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded">
+            Button
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { reactive } from 'vue';
+    import ChildComponent from './ChildComponent.vue';
+
+    const persons = reactive({
+            'name'  : 'Jane Doe',
+            'age'   : 30,
+            'occupasion' : 'Web Developer',
+            'message'   : 'Hi I\'m a Web Developer.'
+        });
+</script>
+
+<template>
+    <div class="mt-10">
+        <h1 class="text-black-900 text-xl my-5">Parent Component:</h1>
+        <h2 class="text-black text-l ">Name : {{ persons.name }}</h2>
+        <h2 class="text-black text-l ">Age : {{ persons.age }}</h2>
+        <h2 class="text-black text-l ">Occupasion : {{ persons.occupasion }}</h2>
+        <h2 class="text-black text-l ">Message : {{ persons.message }}</h2>
+        <ul class="list-disc">
+            <li class="ml-5" v-for="(project, index) in persons.projects" :key="index">{{ project.name }}</li>
+        </ul>
+    </div>
+
+    <ChildComponent v-model:message="persons.message" ></ChildComponent>
+</template>
+
+<style scoped>
+
+</style>
